5527b7b1b5c2fd12304c7f0abc579f36c53fa756
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2014-12-10 12:49:56 +0100

    2.0.1 - Xcode Targets for all projects

---
+++ Source/human-robot-interaction/src/nite-gesture-wave/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nite-gesture-wave
+//
+//  Created by Aravinth Panchadcharam on 10/12/14.
+//  Copyright (c) 2014 Aravinth Panchadcharam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
