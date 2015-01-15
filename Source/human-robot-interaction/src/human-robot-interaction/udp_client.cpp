b23e5a205768ea02410031eabaf02f8ebccf2cde
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2015-01-15 17:25:38 +0100

    4.3.8 - std::to_string patch for NAO Os

--- Source/human-robot-interaction/src/human-robot-interaction/udp_client.cpp
+++ Source/human-robot-interaction/src/human-robot-interaction/udp_client.cpp
@@ -11,6 +11,21 @@
 #include "udp_client.h"
 
 
+#include<sstream>
+template <typename T>
+std::string to_string(T value)
+{
+    //create an output string stream
+    std::ostringstream os ;
+    
+    //throw the value into the string stream
+    os << value ;
+    
+    //convert the string stream into a string and return
+    return os.str() ;
+}
+
+
 /**
  * Constructor
  *
