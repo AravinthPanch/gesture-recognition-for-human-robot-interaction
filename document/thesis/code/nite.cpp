0ec42c512246778f60818db3b761cc69e2c86e29
Aravinth Panchadcharam <panch.aravinth@gmail.com>
2015-06-17 11:43:56 +0200

    9.0.4 - Added: Draft gdoc

---
+++ document/thesis/code/nite.cpp
@@ -0,0 +1,55 @@
+/**
+ * Author: Aravinth Panchadcharam
+ * Email: me@aravinth.info
+ * Date: 28/12/14.
+ * Project: Gesture Recogntion for Human-Robot Interaction
+ */
+
+#include <iostream>
+#include "NiTE.h"
+
+int main(){
+    nite::HandTracker handTracker;
+    nite::NiTE::initialize();
+    handTracker.create();    
+    handTracker.startGestureDetection(nite::GESTURE_WAVE);
+    handTracker.startGestureDetection(nite::GESTURE_CLICK);
+    nite::HandTrackerFrameRef handTrackerFrame;
+    
+    while (true)
+    {
+        handTracker.readFrame(&handTrackerFrame);
+        const nite::Array<nite::GestureData>& gestures = handTrackerFrame.getGestures();
+        
+        for (int i = 0; i < gestures.getSize(); ++i)
+        {
+            if (gestures[i].isComplete())
+            {
+                printf ("Gesture Type %d \n", gestures[i].getType());
+
+                nite::HandId newId;
+                handTracker.startHandTracking(gestures[i].getCurrentPosition(), &newId);
+            }
+        }
+        
+        const nite::Array<nite::HandData>& hands = handTrackerFrame.getHands();
+        for (int i = 0; i < hands.getSize(); ++i)
+        {
+            const nite::HandData& hand = hands[i];
+            if (hand.isTracking())
+            {
+                printf ("Hand at x : %f, y : %f, z : %f \n", hand.getPosition().x, hand.getPosition().y, hand.getPosition().z);
+            }
+        }        
+    }
+    
+    nite::NiTE::shutdown();
+
+    return 0;
+}
+
+
+
+
+
+
---
+++ document/thesis/code/openni.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <OpenNI.h>
+
+#define SAMPLE_READ_WAIT_TIMEOUT 2000
+
+using namespace openni;
+
+int main()
+{
+	OpenNI::initialize();
+	Device depth_camera;
+	depth_camera.open(ANY_DEVICE);
+	
+	VideoStream depth_stream;	
+	depth_stream.create(depth_camera, SENSOR_DEPTH);
+	depth_stream.start();
+	
+	VideoFrameRef depth_frame;
+
+	while (true)
+	{
+		int changedStreamDummy;
+		VideoStream* pStream = &depth_camera;
+		OpenNI::waitForAnyStream(&pStream, 1, &changedStreamDummy, SAMPLE_READ_WAIT_TIMEOUT);
+		
+		depth_camera.readFrame(&depth_frame);				
+		DepthPixel* pDepth = (DepthPixel*)depth_frame.getData();
+		int middleIndex = (depth_frame.getHeight()+1)*depth_frame.getWidth()/2;
+
+		printf("%8d\n", pDepth[middleIndex]);
+	}
+
+	depth_camera.stop();
+	depth_camera.destroy();
+	depth_camera.close();
+	OpenNI::shutdown();
+
+	return 0;
+}
